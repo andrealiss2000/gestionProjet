11b3ce323a3e83a67eddf8b7a7d8696eef0cc8f1
andrealiss2000 <andrealiss@hotmail.fr>
2021-04-13 11:36:24 +0200

    script sql de la base de donnees UFR

---
+++ scriptSql/bdd_ufr.sql
@@ -0,0 +1,80 @@
+CREATE TABLE COMPTE(
+idCompte INTEGER  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+pseudoCompte VARCHAR(15) NOT NULL,
+pwdCompte VARCHAR(40) NOT NULL,  
+adminCompte BOOLEAN NOT NULL
+);
+
+CREATE TABLE LOT(
+idLot INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+idCompte INTEGER,
+FOREIGN KEY (idCompte) REFERENCES COMPTE(idCompte)
+);
+
+
+CREATE TABLE VAGUE(
+idVague INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+horaire DATE
+);
+
+CREATE TABLE LOT_VAGUE(
+idVague INTEGER,
+idLot INTEGER,
+PRIMARY KEY(idVague,idLot), 
+FOREIGN KEY (idVague) REFERENCES VAGUE (idVague),
+FOREIGN KEY (idLot) REFERENCES LOT(idLot)
+);
+
+
+CREATE TABLE ETUDIANT(
+idEtudiant INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nom VARCHAR(20) NOT NULL, 
+prenom VARCHAR(20) NOT NULL,
+resultat_final VARCHAR(10),
+numTD VARCHAR(5)
+);
+
+CREATE TABLE DOMAINE(
+libelleDomaine VARCHAR(30),
+PRIMARY KEY(libelleDomaine)
+);
+
+CREATE TABLE SOUS_DOMAINE(
+libelleSousDomaine VARCHAR(30),
+libelleDomaine VARCHAR(30),
+PRIMARY KEY(libelleSousDomaine),
+FOREIGN KEY (libelleDomaine) REFERENCES DOMAINE(libelleDomaine)
+);
+
+CREATE TABLE COPIE(
+idCopie INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+idLot INTEGER,
+idEtudiant INTEGER,
+libelleDomaine VARCHAR(30),
+noteDomaine INTEGER, 
+resultat_domaine varchar(10),
+FOREIGN KEY (idLot) REFERENCES LOT(idLot),
+FOREIGN KEY (idEtudiant) REFERENCES ETUDIANT(idEtudiant),	
+FOREIGN KEY (libelleDomaine) REFERENCES DOMAINE(libelleDomaine)
+);
+	
+
+CREATE TABLE SALLE(
+designationSalle VARCHAR(30),
+lieu VARCHAR(30), 
+etage VARCHAR(10),
+batiment VARCHAR(15),
+capaciteOrdinateur INTEGER,
+PRIMARY KEY(designationSalle)
+);
+
+CREATE TABLE ETUDIANT_SALLE(
+designationSalle VARCHAR(30),
+idEtudiant INTEGER,
+PRIMARY KEY(designationSalle,idEtudiant),
+FOREIGN KEY (designationSalle) REFERENCES SALLE(designationSalle),
+FOREIGN KEY (idEtudiant) REFERENCES ETUDIANT(idEtudiant)
+
+);
+
+
