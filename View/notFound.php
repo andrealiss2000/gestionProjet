fa58aeb75e1e497eed05836f7f1600f9a13d6d9c
andrealiss2000 <andrealiss@hotmail.fr>
2021-04-13 15:47:05 +0200

    index.php + création des fichiers controleurs

---
+++ View/notFound.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://loading.io/css/transition.css">
+    <link rel="stylesheet" href="./vue/CSS/dashboard.css">
+    <link rel="stylesheet" href="./vue/CSS/loadingBar.css">
+    <link rel="stylesheet" href="./vue/CSS/homepage.css">
+
+    <title>Not Found</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3 text-center mt-5" id="notFoundDiv">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="ml-3">404 not found</h4>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+           
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+if (isset($_GET['controle']) & isset($_GET['action'])) {
+    $controle = $_GET['controle'];
+    $action = $_GET['action'];
+
+
+    //actions vers les pages principales de l'application
+    if ($action == "adminDashboard" || $action == "correctionCopies") {
+        if (!isset($_SESSION["ID_DB"])) { //si user pas connecté alors on le renvoie vers la page d'authentification
+            $action = "ident";
+        }
+    }
+    //On restreint l'utilisateur a certaines pages selon sont usertype
+    if (isset($_SESSION["user_type"]) && $_SESSION["user_type"] != $controle && $action != "notAllowedPage" || !isset($_SESSION["user_type"]) &&  $action != "ident" && ($controle == "admin" || $controle == "correcteur")) {
+        $url = "./index.php?controle=correcteur&action=notAllowedPage";
+        header("Location:" . $url);
+    }
+    
+
+
+} else { //absence de paramètres : prévoir des valeurs => authentification 
+    $controle = "authController";
+    $action = "ident";
+
+    
+    if (isset($_SESSION["ID_DB"])) {
+
+        $url = "./index.php?controle=" . $controle . "&action=" . $action;
+        header("Location:" . $url);
+    }
+}
+
+//inclure le fichier php de contrôle 
+//et lancer la fonction-action issue de ce fichier.	
+if(file_exists('./Controller/' . $controle . '.php')){
+    require('./Controller/' . $controle . '.php');
+}
+if (function_exists($action)) {
+    $action();
+} else {
+    require("./Controller/error.php");
+    notFound();
+}
